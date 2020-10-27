c6213abfd6cf5d2605219e7f237d280cd6f65fb9
nazrul7539 <73455778+nazrul7539@users.noreply.github.com>
2020-10-26 23:16:45 -0400

    Add files via upload

---
+++ src/Java_DataType/DataType01.java
@@ -0,0 +1,26 @@
+package Java_DataType;
+
+public class DataType01 {
+
+	public static void main (String [] args ){
+		
+		int a = 10;
+		int b = 20;
+		//int c = a+b;
+		a = 25;
+		
+		//System.out.println(c);
+		System.out.println(b-a);
+		System.out.println(a-b);
+		System.out.println("*****New Value of C******");
+		System.out.println(a);
+		
+		
+		char c = '5';
+		char h = '&&';
+		
+		System.out.println(c);
+		
+		
+	}
+}
